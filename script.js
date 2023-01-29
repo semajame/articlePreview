5be944dcda41eac0d0c37b3ef096dea4e8588294
James <119842373+semajame@users.noreply.github.com>
2023-01-29 16:24:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function share(){
+    let button = document.querySelector('.share_socials');
+
+    button.classList.toggle('active');
+}
+
+
+function mobile_share(){
+    let button = document.querySelector('.mobile_socials');
+
+    button.classList.toggle('active');
+}
